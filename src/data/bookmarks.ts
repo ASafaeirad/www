52980757c5d168a186549a58c905e5f6f45d5384
MAZE <remvze@proton.me>
2025-11-20 14:58:44 +0330

    Add bookmarks section

---
+++ src/data/bookmarks.ts
@@ -0,0 +1,32 @@
+interface Bookmark {
+  title: string;
+  url: string;
+  date: string;
+}
+
+/**
+ * From old to new
+ */
+
+export const bookmarks: Bookmark[] = [
+  {
+    title: "Bookmark One",
+    date: "2025-11-18",
+    url: "#",
+  },
+  {
+    title: "Bookmark Two",
+    date: "2025-11-19",
+    url: "#",
+  },
+  {
+    title: "Bookmark Three",
+    date: "2025-11-20",
+    url: "#",
+  },
+  {
+    title: "Bookmark Four",
+    date: "2025-11-20",
+    url: "#",
+  },
+];
