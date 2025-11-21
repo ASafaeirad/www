66bb9ad722ed14077285598d90fa4fc18a69c442
MAZE <remvze@proton.me>
2025-11-21 20:33:37 +0330

    Add blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
