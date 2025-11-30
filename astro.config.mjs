b70e74b426237a0db5979f5d3f3792808fcc1955
MAZE <remvze@proton.me>
2025-11-30 21:44:53 +0330

    Change layout and add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
+  site: "https://remvze.com",
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
